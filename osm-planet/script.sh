0e3c75796c3080125792f862b977c6f9e07e8060
Victor Shcherb <victor.shcherb@gmail.com>
2013-02-27 18:56:03 +0100

    Fix script

--- osm-planet/script.sh
+++ osm-planet/script.sh
@@ -11,6 +11,7 @@ function convert {
 	for i in  ${@:3} 
 	do
 		echo "Extracting $i country from $1 $(date) ..."
+		rm "$EXTRACT_DIR"/$i.pbf
 		time osmconvert $1 -B=$2/$i.poly --complex-ways --complete-ways --drop-author -o="$EXTRACT_DIR"/$i.pbf
 	done;
 }
@@ -18,23 +19,34 @@ function convert {
 # 1. AMERICAS
 AMERICAS=""
 #South America
-AMERICAS+="guyana""paraguay""peru""suriname""venezuela" 
-#_NorthAmerica
-AMERICAS+=" bermuda greenland"
+SOUTH_AMERICA="guyana paraguay peru suriname venezuela" 
+convert $PLANET_FILE "geo-polygons/" "south-america"
+convert "$EXTRACT_DIR"/south-america.pbf  "polygons/americas" $SOUTH_AMERICA
+
+#NorthAmerica
+NORTH_AMERICA=" bermuda greenland"
+# TODO north america too big for 2 small countries
+#convert $PLANET_FILE "geo-polygons/" "north-america"
+#convert "$EXTRACT_DIR"/north-america.pbf  "polygons/americas" $NORTH_AMERICA
+
+
 # Central America
-AMERICAS+=" costa_rica el_salvador honduras nicaragua panama"
+convert $PLANET_FILE "geo-polygons/" "central-america.poly"
+
+CENTRAL_AMERICA=" costa_rica el_salvador honduras nicaragua panama"
+convert "$EXTRACT_DIR"/central-america.poly.pbf  "polygons/americas" $CENTRAL_AMERICA
+
 # Carribean (suffix = _CentralAmerica)
-AMERICAS+=" anguilla antigua_and_barbuda aruba 
+CARRIBEAN=" anguilla antigua_and_barbuda aruba 
           bahamas barbados british_virgin_islands 
           cayman_islands cuba dominica dominican_republic 
           haiti jamaica grenada guadeloupe martinique 
           montserrat netherlands_antilles puerto_rico  
           saint_vincent_and_the_grenadines trinidad_and_tobago virgin_islands_us"
-
-# convert "$PLANET_FILE" "polygons/americas/" $AMERICAS
+convert "$EXTRACT_DIR"/central-america.poly.pbf  "polygons/americas" $CARRIBEAN
 
 # 2. AFRICA Geofabrik
-# convert $PLANET_FILE "geo-polygons/" "africa"
+convert $PLANET_FILE "geo-polygons/" "africa"
 AFRICA_GEOFABRIK="burkina_faso canary_islands ethiopia guinea guinea-bissau ivory_coast 
 				liberia libya madagascar morocco nigeria somalia south_africa_and_lesotho tanzania "
 convert "$EXTRACT_DIR"/africa.pbf "geo-polygons/africa/" $AFRICA_GEOFABRIK
@@ -44,9 +56,9 @@ ASIA="";
 ASIA+=" hong_kong macao north_korea south_korea"
 ASIA+=" brunei cambodia christmas_island east_timor laos malaysia myanmar singapore spratly_islands thailand vietnam"
 ASIA+=" afghanistan bangladesh bhutan british_indian_ocean_territory iran maldives nepal sri_lanka"
-ASIA+=" armenia bahrain caspian_sea cyprus georgia jordan kuwait lebanon oman qatar saudi_arabia syria turkey united_arab_emirates yemen"
+ASIA+=" armenia bahrain cyprus georgia jordan kuwait lebanon oman qatar saudi_arabia syria turkey united_arab_emirates yemen"
 
-# convert $PLANET_FILE "geo-polygons/" "asia"
+convert $PLANET_FILE "geo-polygons/" "asia"
 convert "$EXTRACT_DIR"/asia.pbf "polygons/asia/" $ASIA
 
 
