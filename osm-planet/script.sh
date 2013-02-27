3c6de08678500d64fd81da7d9ac666c46d0743e5
Victor Shcherb <victor.shcherb@gmail.com>
2013-02-27 18:43:30 +0100

    Fix script

--- osm-planet/script.sh
+++ osm-planet/script.sh
@@ -8,10 +8,10 @@ fi
 mkdir -p "$EXTRACT_DIR"
 
 function convert {
-	for i in $1
+	for i in  ${@:3} 
 	do
-		echo "Extracting $i country from $2 $(date) ..."
-		time osmconvert $2 -B=$3/$i.poly --complex-ways --complete-ways --drop-author -o="$EXTRACT_DIR"/$i.pbf
+		echo "Extracting $i country from $1 $(date) ..."
+		time osmconvert $1 -B=$2/$i.poly --complex-ways --complete-ways --drop-author -o="$EXTRACT_DIR"/$i.pbf
 	done;
 }
 
@@ -31,13 +31,13 @@ AMERICAS+=" anguilla antigua_and_barbuda aruba
           montserrat netherlands_antilles puerto_rico  
           saint_vincent_and_the_grenadines trinidad_and_tobago virgin_islands_us"
 
-# convert $AMERICAS "$PLANET_FILE" "polygons/americas/"
+# convert "$PLANET_FILE" "polygons/americas/" $AMERICAS
 
 # 2. AFRICA Geofabrik
-convert "africa" $PLANET_FILE "geo-polygons/"
+# convert $PLANET_FILE "geo-polygons/" "africa"
 AFRICA_GEOFABRIK="burkina_faso canary_islands ethiopia guinea guinea-bissau ivory_coast 
 				liberia libya madagascar morocco nigeria somalia south_africa_and_lesotho tanzania "
-convert $AFRICA_GEOFABRIK "$EXTRACT_DIR"/africa.pbf "geo-polygons/africa/"
+convert "$EXTRACT_DIR"/africa.pbf "geo-polygons/africa/" $AFRICA_GEOFABRIK
 
 # 3. ASIA
 ASIA="";
@@ -46,7 +46,7 @@ ASIA+=" brunei cambodia christmas_island east_timor laos malaysia myanmar singap
 ASIA+=" afghanistan bangladesh bhutan british_indian_ocean_territory iran maldives nepal sri_lanka"
 ASIA+=" armenia bahrain caspian_sea cyprus georgia jordan kuwait lebanon oman qatar saudi_arabia syria turkey united_arab_emirates yemen"
 
-convert "asia" $PLANET_FILE "geo-polygons/"
-convert $ASIA  "$EXTRACT_DIR"/asia.pbf "polygons/asia/"
+# convert $PLANET_FILE "geo-polygons/" "asia"
+convert "$EXTRACT_DIR"/asia.pbf "polygons/asia/" $ASIA
 
 
