6f226fafcc89449b81eaa24d181e5dee47d921bb
Victor Shcherb <victor.shcherb@gmail.com>
2013-06-01 16:32:38 +0200

    Fix script

--- copy_downloads_to_sync.sh
+++ copy_downloads_to_sync.sh
@@ -2,7 +2,7 @@
 DIRECTORY=$(cd `dirname $0` && pwd)
 
 INDEXES_DIR=/var/www-download/indexes/
-SYNC_DIR=/var/www-download/sync/
+SYNC_DIR=/var/sync/
 
 # R2NGOYG7TSJL7BT5QB5QFULORCH7TQ43X
 mkdir -p $SYNC_DIR/Russia
@@ -17,8 +17,10 @@ rsync --progress --delete-after --times  $INDEXES_DIR/Gb_* $SYNC_DIR/GB/
 mkdir -p $SYNC_DIR/Germany
 rsync --progress --delete-after --times  $INDEXES_DIR/Germany_* $SYNC_DIR/Germany/
 
+# R7MSXJBL7PDB74LI3A5HTIUQ7QDQE5ITU
 mkdir -p $SYNC_DIR/Europe
 rsync --progress --delete-after --times --exclude="(France|Gb_|Germany)*" $INDEXES_DIR/*_europe_* $SYNC_DIR/Europe/
 
+# RJ6BUYMK4CDT64G3JZWVKAK2UPNGALXXI
 mkdir -p $SYNC_DIR/NorthAmerica
 rsync --progress --delete-after --times $INDEXES_DIR/*_northamerica_* $SYNC_DIR/NorthAmerica/
