dd4f467a242c4995772a3516ade4b0c3d0d5a400
Victor Shcherb <victor.shcherb@gmail.com>
2013-06-01 16:17:10 +0200

    Fix script

--- copy_downloads_to_sync.sh
+++ copy_downloads_to_sync.sh
@@ -5,8 +5,11 @@ INDEXES_DIR=/var/www-download/indexes/
 SYNC_DIR=/var/www-download/sync/
 
 # R2NGOYG7TSJL7BT5QB5QFULORCH7TQ43X
+mkdir -p $SYNC_DIR/Russia
 rsync --progress --include=Russia_* --exclude=* $INDEXES_DIR/* $SYNC_DIR/Russia/
 
+mkdir -p $SYNC_DIR/Europe
 rsync --progress --include=*_europe_* --exclude=* $INDEXES_DIR/* $SYNC_DIR/Europe/
 
+mkdir -p $SYNC_DIR/NorthAmerica
 rsync --progress --include=*_northamerica_* --exclude=* $INDEXES_DIR/* $SYNC_DIR/NorthAmerica/
