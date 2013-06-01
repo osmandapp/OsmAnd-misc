a0172dffc23e787ffb0a6bf94ae38714699f0889
Victor Shcherb <victor.shcherb@gmail.com>
2013-06-01 16:12:52 +0200

    Fix script

--- copy_downloads_to_sync.sh
+++ copy_downloads_to_sync.sh
@@ -5,8 +5,8 @@ INDEXES_DIR=/var/www-download/indexes/
 SYNC_DIR=/var/www-download/sync/
 
 # R2NGOYG7TSJL7BT5QB5QFULORCH7TQ43X
-rsync --include=*Russia_* $INDEXES_DIR $SYNC_DIR/Russia
+rsync --progress --include=Russia_* --exclude=* $INDEXES_DIR/* $SYNC_DIR/Russia/
 
-rsync --include=*_europe_* $INDEXES_DIR $SYNC_DIR/Europe
+rsync --progress --include=*_europe_* --exclude=* $INDEXES_DIR/* $SYNC_DIR/Europe/
 
-rsync --include=*_northamerica_* $INDEXES_DIR $SYNC_DIR/NorthAmerica
+rsync --progress --include=*_northamerica_* --exclude=* $INDEXES_DIR/* $SYNC_DIR/NorthAmerica/
