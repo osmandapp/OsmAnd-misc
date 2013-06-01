d63cdc91baac988fa66e50f78c4f8ec1b0e224e9
Victor Shcherb <victor.shcherb@gmail.com>
2013-06-01 16:25:46 +0200

    Fix script

--- copy_downloads_to_sync.sh
+++ copy_downloads_to_sync.sh
@@ -6,10 +6,19 @@ SYNC_DIR=/var/www-download/sync/
 
 # R2NGOYG7TSJL7BT5QB5QFULORCH7TQ43X
 mkdir -p $SYNC_DIR/Russia
-rsync --progress --delete-after --times --include=Russia_* --exclude=* $INDEXES_DIR/* $SYNC_DIR/Russia/
+rsync --progress --delete-after --times $INDEXES_DIR/Russia_* $SYNC_DIR/Russia/
+
+mkdir -p $SYNC_DIR/France
+rsync --progress --delete-after --times  $INDEXES_DIR/France* $SYNC_DIR/France/
+
+mkdir -p $SYNC_DIR/GB
+rsync --progress --delete-after --times  $INDEXES_DIR/Gb_* $SYNC_DIR/GB/
+
+mkdir -p $SYNC_DIR/Germany
+rsync --progress --delete-after --times  $INDEXES_DIR/Germany_* $SYNC_DIR/Germany/
 
 mkdir -p $SYNC_DIR/Europe
-rsync --progress --delete-after --times  --include=*_europe_* --exclude=* $INDEXES_DIR/* $SYNC_DIR/Europe/
+rsync --progress --delete-after --times --exclude="(France|Gb_|Germany)*" $INDEXES_DIR/*_europe_* $SYNC_DIR/Europe/
 
 mkdir -p $SYNC_DIR/NorthAmerica
-rsync --progress --delete-after --times  --include=*_northamerica_* --exclude=* $INDEXES_DIR/* $SYNC_DIR/NorthAmerica/
+rsync --progress --delete-after --times $INDEXES_DIR/*_northamerica_* $SYNC_DIR/NorthAmerica/
