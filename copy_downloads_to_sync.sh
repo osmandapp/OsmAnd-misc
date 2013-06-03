883eec1a4ba4b4e37455c53c79b0f48a1b79f3cc
Victor Shcherb <victor.shcherb@gmail.com>
2013-06-04 00:02:31 +0200

    Add indexes

--- copy_downloads_to_sync.sh
+++ copy_downloads_to_sync.sh
@@ -8,12 +8,15 @@ SYNC_DIR=/var/sync/
 mkdir -p $SYNC_DIR/Russia
 rsync --progress --delete-after --dirs --times $INDEXES_DIR/Russia_* $SYNC_DIR/Russia/
 
+#RYL64R2O3EKFBXKX3Z4C6NRT374WLMJD7
 mkdir -p $SYNC_DIR/France
 rsync --progress --delete-after --dirs --times  $INDEXES_DIR/France* $SYNC_DIR/France/
 
+#RW2VAHTU62NV5W5CLC5YS6XGXWHSEKTIA
 mkdir -p $SYNC_DIR/GB
 rsync --progress --delete-after --dirs --times  $INDEXES_DIR/Gb_* $SYNC_DIR/GB/
 
+#RAMO45FOHSUHZNCIPEJX4EMDPOW44HOT6
 mkdir -p $SYNC_DIR/Germany
 rsync --progress --delete-after --dirs --times  $INDEXES_DIR/Germany_* $SYNC_DIR/Germany/
 
