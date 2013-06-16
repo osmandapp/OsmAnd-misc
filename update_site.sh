2aa2b14557bf42ffd928909db598fca01b6211d4
Victor Shcherb <victor.shcherb@gmail.com>
2013-06-17 01:03:20 +0200

    update countries

--- update_site.sh
+++ update_site.sh
@@ -7,7 +7,7 @@ LOCAL_SITE_DIR=/var/www-download/
 mkdir -p $LOCAL_SITE_DIR/
 cp -vur $GIT_SITE_DIR/* $LOCAL_SITE_DIR && \
 cp -vu $GIT_SITE_DIR/../../resources/countries-info/countries.xml $LOCAL_SITE_DIR/countries.xml && \
-chgrp -R www-data $LOCAL_SITE_DIR/
+chgrp -R www-data $LOCAL_SITE_DIR/*
 #files='*.php tile_sources.xml favicon.ico'
 #for f in $files ; do
 #	cp $GIT_SITE_DIR/$f $LOCAL_SITE_DIR/ -u;
