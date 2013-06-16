7ac13fd16d05d4df625bbafb9497017ab2c1b858
Victor Shcherb <victor.shcherb@gmail.com>
2013-06-16 17:14:08 +0200

    Refactor sync server

---
+++ sync_downloads_with_main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+function sync {
+	rsync --progress --delete-after --dirs --times jenkins@download.osmand.net:$1 $1
+}
+sync /var/www-download/indexes
+sync /var/www-download/hillshade
+sync /var/www-download/srtm
+sync /var/www-download/road-indexes
+
