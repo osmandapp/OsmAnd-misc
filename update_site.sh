ee9f6f0d591021028bebbd50b6aa7ca50e35c2ee
Victor Shcherb <victor.shcherb@gmail.com>
2013-06-17 00:26:17 +0200

    Update site

--- update_site.sh
+++ update_site.sh
@@ -5,7 +5,7 @@ GIT_SITE_DIR=$DIRECTORY/site/*
 LOCAL_SITE_DIR=/var/www-download/
 
 mkdir -p $LOCAL_SITE_DIR/
-cp -vurL $GIT_SITE_DIR $LOCAL_SITE_DIR  
+cp -vurL --no-preserve=links $GIT_SITE_DIR $LOCAL_SITE_DIR  
 #files='*.php tile_sources.xml favicon.ico'
 #for f in $files ; do
 #	cp $GIT_SITE_DIR/$f $LOCAL_SITE_DIR/ -u;
