8aaf0376a12525ca92e380025ad5652609562db9
Victor Shcherb <victor.shcherb@gmail.com>
2013-06-17 00:11:54 +0200

    Full site update

--- update_site.sh
+++ update_site.sh
@@ -4,13 +4,9 @@ DIRECTORY=$(cd `dirname $0` && pwd)
 GIT_SITE_DIR=$DIRECTORY/site
 LOCAL_SITE_DIR=/var/www-download
 
-files='*.php tile_sources.xml favicon.ico'
-for f in $files ; do
-	cp $GIT_SITE_DIR/$f $LOCAL_SITE_DIR/ -u;
-done
-
-mkdir -p $LOCAL_SITE_DIR/resource;
-cp $GIT_SITE_DIR/resource/* $LOCAL_SITE_DIR/resource/ -u;
-mkdir -p $LOCAL_SITE_DIR/GoogleAnalytics;
-cp $GIT_SITE_DIR/GoogleAnalytics/* $LOCAL_SITE_DIR/GoogleAnalytics/ -ru;
-
+mkdir -p $LOCAL_SITE_DIR/
+cp -vur $GIT_SITE_DIR $LOCAL_SITE_DIR  
+#files='*.php tile_sources.xml favicon.ico'
+#for f in $files ; do
+#	cp $GIT_SITE_DIR/$f $LOCAL_SITE_DIR/ -u;
+#done
