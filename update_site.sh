b9ac1ca5d0ae7c3122d72f97b6ed33de3f044dfd
Victor Shcherb <victor.shcherb@gmail.com>
2013-06-21 23:36:26 +0200

    Update site

--- update_site.sh
+++ update_site.sh
@@ -8,6 +8,7 @@ mkdir -p $LOCAL_SITE_DIR/
 cp -vur $GIT_SITE_DIR/* $LOCAL_SITE_DIR && \
 cp -vu $GIT_SITE_DIR/../../resources/countries-info/countries.xml $LOCAL_SITE_DIR/countries.xml && \
 chgrp -R www-data $LOCAL_SITE_DIR/*
+chmod g+w indexes.xml 
 #files='*.php tile_sources.xml favicon.ico'
 #for f in $files ; do
 #	cp $GIT_SITE_DIR/$f $LOCAL_SITE_DIR/ -u;
