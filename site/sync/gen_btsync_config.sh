22c52ef04f1b45058ca5c27193817af55e3f1a43
Victor Shcherb <victor.shcherb@gmail.com>
2013-06-26 22:30:23 +0200

    Update copy downloads

--- site/sync/gen_btsync_config.sh
+++ site/sync/gen_btsync_config.sh
@@ -2,11 +2,15 @@
 DIRECTORY=$(cd `dirname $0` && pwd)
 mkdir -p $DIRECTORY/publish
 cat  $DIRECTORY/server_template.config
+RUN_IN=`pwd`
 # echo '"shared_folders" : ['
 for f in *
 do
-  SECRET=`$DIRECTORY/btsync --generate-secret`
-  RO_SECRET=`$DIRECTORY/btsync --get-ro-secret $SECRET`
-  echo "{ \"secret\": \"$SECRET\", \"dir\": \"$DIRECTORY/publish/$f\", \"ro-secret\" : \"$RO_SECRET\" \"use_relay_server\" : true,\"use_tracker\" : true,  \"use_dht\" : true,\"search_lan\" : true}, "
+  if [ -d "$RUN_IN/$f" ]
+  then
+  	SECRET=`$DIRECTORY/btsync --generate-secret`
+  	RO_SECRET=`$DIRECTORY/btsync --get-ro-secret $SECRET`
+  	echo "{ \"secret\": \"$SECRET\", \"dir\": \"$RUN_IN/$f\", \"ro-secret\" : \"$RO_SECRET\" \"use_relay_server\" : true,\"use_tracker\" : true,  \"use_dht\" : true,\"search_lan\" : true}, "
+  fi
 done
 echo '] }'
