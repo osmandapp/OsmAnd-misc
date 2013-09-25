4204e00de1e4d0ea8b0f837e5bb6471b25cb40c7
vshcherb <victor.shcherb@gmail.com>
2013-09-25 13:54:13 +0200

    Add gzip to indexes

--- site/get_indexes.php
+++ site/get_indexes.php
@@ -1,8 +1,13 @@
 <?php 
    include 'update_indexes.php';
    updateIndexes();
-   header('Content-type: application/xml');
-   header('Content-Disposition: attachment; filename="indexes.xml"');
-
-   readfile('indexes.xml');
+   if(isset($_GET['gzip'])){
+    	header("Content-type: gzip");
+    	header('Content-Disposition: attachment; filename="indexes.xml.gz"');
+    	echo gzencode(file_get_contents('indexes.xml'));
+	} else {
+   		header('Content-type: application/xml');
+   		header('Content-Disposition: attachment; filename="indexes.xml"');
+   		readfile('indexes.xml');
+   }
 ?>
