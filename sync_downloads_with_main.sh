3504510e418ed875b3fd095da4d6d111870ac30d
vshcherb <victor.shcherb@gmail.com>
2014-05-07 18:17:20 +0200

    Update sync_downloads_with_main.sh

--- sync_downloads_with_main.sh
+++ sync_downloads_with_main.sh
@@ -4,7 +4,6 @@ function sync {
 }
 sync /var/www-download/indexes/
 sync /var/www-download/hillshade/
-sync /var/www-download/srtm/
 sync /var/www-download/srtm-countries/
 sync /var/www-download/road-indexes/
 sync /var/www-download/tours/
