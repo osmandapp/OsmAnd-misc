4eca0cb39d0ee81c6fbf2cc25d05193f5eb546d2
vshcherb <victor.shcherb@gmail.com>
2014-05-10 01:07:23 +0300

    Fix update mapnik

--- config/mapnik_update.sh
+++ config/mapnik_update.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
-ID=_`(date +"%d_%m_%H_%M")`
-/var/lib/postgresql/osmosis.run --rri workingDirectory=/var/lib/postgresql/osmosis-workdir --simplify-change --write-xml-change changes$ID.osc.gz
-# /var/lib/postgresql/osmosis.run --rri workingDirectory=/var/lib/postgresql/osmosis-workdir --simc --wx - \
-#| osm2pgsql --append --style /var/lib/postgresql/osm2pgsql/osm2pgsql/default.style --number-processes 4 -d gis --slim -k --expire-tiles 13-18 --expire-output /var/lib/postgresql/expired_tiles.list
+ID=`(date +"%d_%m_%H_%M")`
+$FOLDER/osmosis.run --rri workingDirectory=$FOLDER/osmosis-workdir --simplify-change --write-xml-change $FOLDER/changes$ID.osc.gz
 
-osm2pgsql --append --style /var/lib/postgresql/osm2pgsql/osm2pgsql/default.style --number-processes 4 -d gis --slim  --expire-tiles 13-18 --expire-output /var/lib/postgresql/expired_tiles$ID.list changes$ID.osc.gz
-ls -larh changes$ID.osc.gz
-rm changes$ID.osc.gz
+osm2pgsql --append --style /usr/local/share/osm2pgsql/default.style \
+-k --flat-nodes /postgresql/flatnodes \
+--number-processes 4 -C 25000 -d gis --slim  --expire-tiles 13-18 \
+--expire-output $FOLDER/expired_tiles$ID.list $FOLDER/changes$ID.osc.gz
 
-cat /var/lib/postgresql/expired_tiles$ID.list | /var/lib/postgresql/mod_tile/render_expired --min-zoom=13
-rm expired_tiles$ID.list
+ls -larh $FOLDER/changes$ID.osc.gz
+rm $FOLDER/changes$ID.osc.gz
+
+bzip2 $FOLDER/expired_tiles$ID.list
+# bzcat $FOLDER/expired_tiles$ID.list.bz2 | $FOLDER/mod_tile/render_expired --touch-from=13 --min-zoom=13
+#rm $FOLDER/expired_tiles$ID.list
