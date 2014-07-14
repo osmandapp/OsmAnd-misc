019dc80f149ae45d87c33890d3f82be764ac4c8d
Victor Shcherb <victor.shcherb@gmail.com>
2014-07-15 00:45:22 +0200

    Add script to update regions

--- osm-planet/updateRegions.sh
+++ osm-planet/updateRegions.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 BASE=/var/lib/jenkins/osm-planet/regions
+
 updateRegion() {
 	echo "Update ${1} using ${2}"
 	if 
-    osmupdate ${BASE}/${1}.o5m ${BASE}/current-update.o5m -B=misc/osm-planet/polygons${2}-v
+    osmupdate ${BASE}/${1}.o5m ${BASE}/current-update.o5m -B=misc/osm-planet/polygons${2} -v
     osmconvert --out-pbf ${BASE}/${1}.o5m > ${BASE}/${1}.osm.pbf
     # mv -f ${1} ${1}.old || true
     rm ${BASE}/${1}
     mv ${BASE}/current-update.o5m ${BASE}/${1}
 }
+
 rm current-update.o5m || true
 updateRegion Russia-sankt-petersburg russia-regions/sankt-peterburg.poly
 updateRegion Russia-moscow russia-regions/moscow.poly
