20b278b4a7fcdd77513b4092f361b8cb491e8571
Victor Shcherb <victor.shcherb@gmail.com>
2014-07-15 00:49:43 +0200

    Add script to update regions

--- osm-planet/updateRegions.sh
+++ osm-planet/updateRegions.sh
@@ -5,9 +5,9 @@ updateRegion() {
 	echo "Update ${1} using ${2}"
     osmupdate ${BASE}/${1}.o5m ${BASE}/current-update.o5m -B=misc/osm-planet/polygons/${2} -v
     osmconvert --out-pbf ${BASE}/${1}.o5m > ${BASE}/${1}.osm.pbf
-    # mv -f ${1} ${1}.old || true
-    rm ${BASE}/${1}
-    mv ${BASE}/current-update.o5m ${BASE}/${1}
+    mv -f ${1} ${1}.o5m.old || true
+    # rm ${BASE}/${1}.o5m
+    mv ${BASE}/current-update.o5m ${BASE}/${1}.o5m
 }
 
 rm current-update.o5m || true
