ca0126e563748c0bff96bd1564ba7bf1d83a16e6
Victor Shcherb <victor.shcherb@gmail.com>
2014-07-16 13:47:25 +0200

    Fix update regions

--- osm-planet/updateRegions.sh
+++ osm-planet/updateRegions.sh
@@ -6,7 +6,7 @@ updateRegion() {
 	echo "Update ${1} using ${2}"
 	FILE="${BASE}/${1}.o5m"
 	if [ ! -f $FILE ]; then
-		osmconvert --out-o5m "${PBF_FILES}/${3}.pbf" -B=misc/osm-planet/${2} > ${FILE} 
+		osmconvert --out-o5m "${PBF_FILES}/${3}" -B=misc/osm-planet/${2} > ${FILE} 
 	fi
     osmupdate $FILE ${BASE}/current-update.o5m -B=misc/osm-planet/${2} -v
     osmconvert --out-pbf $FILE > ${BASE}/${1}.osm.pbf
