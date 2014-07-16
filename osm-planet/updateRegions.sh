54fa9a4db6eed2deab2440108ed2d0d85158402a
Victor Shcherb <victor.shcherb@gmail.com>
2014-07-16 13:41:46 +0200

    Fix update regions

--- osm-planet/updateRegions.sh
+++ osm-planet/updateRegions.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 BASE=/var/lib/jenkins/osm-planet/regions
+PBF_FILES="/var/lib/jenkins/workspace/Planet_Update-Extract Osm Maps Downloads/extracted/"
 
 updateRegion() {
 	echo "Update ${1} using ${2}"
-    osmupdate ${BASE}/${1}.o5m ${BASE}/current-update.o5m -B=misc/osm-planet/polygons/${2} -v
-    osmconvert --out-pbf ${BASE}/${1}.o5m > ${BASE}/${1}.osm.pbf
-    mv -f ${BASE}/${1}.o5m ${BASE}/${1}.o5m.old || true
+	FILE=${BASE}/${1}.o5m
+	if [ ! -f $FILE ]; then
+		osmconvert --out-o5m ${PBF_FILES}/${3 > ${FILE} -B=misc/osm-planet/${2}	
+	fi
+    osmupdate $FILE ${BASE}/current-update.o5m -B=misc/osm-planet/${2} -v
+    osmconvert --out-pbf $FILE > ${BASE}/${1}.osm.pbf
+    mv -f $FILE ${FILE}.old || true
     # rm ${BASE}/${1}.o5m
-    mv ${BASE}/current-update.o5m ${BASE}/${1}.o5m
+    mv ${BASE}/current-update.o5m $FILE
 }
 
 rm current-update.o5m || true
-updateRegion Russia-sankt-petersburg russia-regions/sankt-peterburg.poly
-updateRegion Russia-moscow russia-regions/moscow.poly
+# Europe
+updateRegion England-london geo-polygons/europe/great-britain/england/greater-london europe.pbf
+
+# Russia
+updateRegion Russia-sankt-petersburg polygons/russia-regions/sankt-peterburg.poly russia.pbf
+updateRegion Russia-moscow polygons/russia-regions/moscow.poly russia.pbf
+updateRegion Russia-moscovskaya-oblast polygons/russia-regions/moskovskaya-oblast.poly russia.pbf
 
 ls -larh ${BASE}
 
