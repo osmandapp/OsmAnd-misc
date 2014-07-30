cf1caa68e9d2d63d37df6353284a0da7945047bb
hvdwolf <hvdwolf@gmail.com>
2014-07-30 23:02:19 +0200

    Update updateRegions.sh
    Add several metropolises

--- osm-planet/updateRegions.sh
+++ osm-planet/updateRegions.sh
@@ -18,6 +18,20 @@ updateRegion() {
 rm current-update.o5m || true
 # Europe
 updateRegion England-london geo-polygons/europe/great-britain/england/greater-london.poly europe.pbf
+updateRegion England-birmingham polygons/europe/gb-metropolises/birmingham.poly europe.pbf
+updateRegion England-leeds polygons/europe/gb-metropolises/leeds.poly europe.pbf
+ 
+updateRegion France-paris polygons/europe/france-metropolises/paris.poly europe.pbf
+updateRegion France-lyon polygons/europe/france-metropolises/lyon.poly europe.pbf
+updateRegion France-marseille polygons/europe/france-metropolises/marseille.poly europe.pbf
+updateRegion France-toulouse polygons/europe/france-metropolises/toulouse.poly europe.pbf
+
+updateRegion Germany-munich polygons/europe/germany-metropolises/munchen.poly europe.pbf
+updateRegion German-ruhregebiet-koln polygons/europe/germany-metropolises/ruhrgebiet-koln.poly europe.pbf
+
+updateRegion Italy-roma polygons/europe/italy-metropolises/italy.poly europe.pbf
+
+updateRegion Spain-madrid polygons/europe/spain-metropolises/madrid.poly europe.pbf
 
 # Russia
 updateRegion Russia-sankt-petersburg polygons/russia-regions/sankt-peterburg.poly russia.pbf
