dcadd8bde4db1a4121e3584426ee5141eb3bc2af
hvdwolf <hvdwolf@gmail.com>
2014-08-09 15:12:13 +0200

    Somehow update regions wasn pushed

--- osm-planet/updateRegions.sh
+++ osm-planet/updateRegions.sh
@@ -18,24 +18,33 @@ updateRegion() {
 rm current-update.o5m || true
 # Europe
 updateRegion England-london geo-polygons/europe/great-britain/england/greater-london.poly europe.pbf
-updateRegion England-birmingham polygons/europe/gb-metropolises/birmingham.poly europe.pbf
-updateRegion England-leeds polygons/europe/gb-metropolises/leeds.poly europe.pbf
+updateRegion England-birmingham metropolis-polygons/europe/great-britain/birmingham.poly europe.pbf
+updateRegion England-leeds metropolis-polygons/europe/great-britain/leeds.poly europe.pbf
  
-updateRegion France-paris polygons/europe/france-metropolises/paris.poly europe.pbf
-updateRegion France-lyon polygons/europe/france-metropolises/lyon.poly europe.pbf
-updateRegion France-marseille polygons/europe/france-metropolises/marseille.poly europe.pbf
-updateRegion France-toulouse polygons/europe/france-metropolises/toulouse.poly europe.pbf
+updateRegion France-paris metropolis-polygons/europe/france/paris.poly europe.pbf
+updateRegion France-lyon metropolis-polygons/europe/france/lyon.poly europe.pbf
+updateRegion France-marseille metropolis-polygons/europe/france/marseille.poly europe.pbf
+updateRegion France-toulouse metropolis-polygons/europe/france/toulouse.poly europe.pbf
 
-updateRegion Germany-munich polygons/europe/germany-metropolises/munchen.poly europe.pbf
-updateRegion German-ruhregebiet-koln polygons/europe/germany-metropolises/ruhrgebiet-koln.poly europe.pbf
+updateRegion Germany-munich metropolis-polygons/europe/germany/munchen.poly europe.pbf
+updateRegion German-ruhregebiet-koln metropolis-polygons/europe/germany/ruhrgebiet-koln.poly europe.pbf
 
-updateRegion Italy-roma polygons/europe/italy-metropolises/italy.poly europe.pbf
+updateRegion Italy-roma metropolis-polygons/europe/italy/italy.poly europe.pbf
 
-updateRegion Spain-madrid polygons/europe/spain-metropolises/madrid.poly europe.pbf
+updateRegion Spain-madrid metropolis-polygons/europe/spain/madrid.poly europe.pbf
+
+updateRegion Sweden-Stockholm metropolis-polygons/europe/sweden/stockholm.poly europe.pbf
 
 # Russia
 updateRegion Russia-sankt-petersburg polygons/russia-regions/sankt-peterburg.poly russia.pbf
 updateRegion Russia-moscow polygons/russia-regions/moscow.poly russia.pbf
 updateRegion Russia-moscovskaya-oblast polygons/russia-regions/moskovskaya-oblast.poly russia.pbf
+
+# United States
+#updateRegion US-chicago-north-america metropolis-polygons/north-america/us/chicago.poly 
+#updateRegion US-houston-north-america metropolis-polygons/north-america/us/houston.poly
+#updateRegion US-los-angeles-north-america metropolis-polygons/north-america/us/los-angeles.poly
+#updateRegion US-new-york-north-america metropolis-polygons/north-america/us/new-york.poly
+
 
 ls -larh ${BASE}
