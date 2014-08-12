592b02e0755bfcff37d03b6879da7b3736892f17
Victor Shcherb <victor.shcherb@gmail.com>
2014-08-13 01:11:10 +0200

    Add misc

--- site/tours.php
+++ site/tours.php
@@ -4,19 +4,8 @@ include 'download_utils.php';
 $code = $_GET['code']; 
 $dom = new DomDocument(); 
 $dom->load('indexes.xml'); 
-$outputIndexes->setAttribute('mapversion','1');
-$output->appendChild( $outputIndexes );
-loadIndexesFromDir($output, $outputIndexes, 'indexes/', 'region', 'tour');
+$outputIndexes = $dom-> getElementsByTagName('indexes');
+loadIndexesFromDir($dom, $outputIndexes, '/var/lib/jenkins/tours/'.$code.'/', 'region', 'tour');
 
-if(isset($_GET['gzip'])){
-   	header("Content-type: gzip");
-   	header('Content-Disposition: attachment; filename="indexes.xml.gz"');
-   	echo gzencode(file_get_contents('indexes.xml'));
-} else {
- 	header('Content-type: application/xml');
- 	header('Content-Disposition: attachment; filename="indexes.xml"');
- 	readfile('indexes.xml');
- }
-	$output->save($localFileName);
-updateIndexes(false);
+$dom->saveXML();
 ?>
