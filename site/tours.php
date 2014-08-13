bc3f5b33c4ae42d2879360cb59fa0f23721c490e
Victor Shcherb <victor.shcherb@gmail.com>
2014-08-13 02:01:00 +0200

    Add misc

--- site/tours.php
+++ site/tours.php
@@ -7,7 +7,7 @@ $dom->load('indexes.xml');
 $outputIndexes = $dom-> getElementsByTagName('osmand_regions')->item(0);
 loadIndexesFromDir($dom, $outputIndexes, '/var/lib/jenkins/tours/'.$code.'/', 'region', 'tour');
 
-header('Content-type: application/xml');
-header('Content-Disposition: attachment; filename="tours.xml"');
-print $dom->saveXML();
+header("Content-type: gzip");
+header('Content-Disposition: attachment; filename="tours.xml.gz"');
+print gzencode($dom->saveXML());
 ?>
