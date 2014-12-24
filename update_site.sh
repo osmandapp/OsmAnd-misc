ff20c8dadc92acb9009460ef293b1959d9310796
Victor Shcherb <victor.shcherb@gmail.com>
2014-12-24 02:24:41 +0100

    Update site

--- update_site.sh
+++ update_site.sh
@@ -5,7 +5,9 @@ GIT_SITE_DIR=$DIRECTORY/site/
 LOCAL_SITE_DIR=/var/www-download/
 
 mkdir -p $LOCAL_SITE_DIR/
+mkdir -p $LOCAL_SITE_DIR/static/
 cp -vur $GIT_SITE_DIR/* $LOCAL_SITE_DIR && \
+cp -vur ../help/website/* $LOCAL_SITE_DIR/static/ && \
 cp -vu $GIT_SITE_DIR/../../resources/countries-info/countries.xml $LOCAL_SITE_DIR/countries.xml && \
 chgrp -R www-data $LOCAL_SITE_DIR/*
 chmod g+w $LOCAL_SITE_DIR/indexes.xml 
