93f91f361809b3e73ad12024e1763e6374b78d6f
vshcherb <victor.shcherb@gmail.com>
2015-02-12 23:52:45 +0100

    Update update_site.sh

--- update_site.sh
+++ update_site.sh
@@ -11,7 +11,10 @@ cp -vur $GIT_SITE_DIR/* $LOCAL_SITE_DIR
 cp -vu $GIT_SITE_DIR/../../resources/countries-info/countries.xml $LOCAL_SITE_DIR/countries.xml
 cp -vur ../help/website/* $LOCAL_SITE_DIR
 chgrp -R www-data $LOCAL_SITE_DIR/*
+
 chmod g+w $LOCAL_SITE_DIR/indexes.xml 
 
 cp -vur ../help/dvr/* $DVR_SITE_DIR
+chgrp -R www-data $LOCAL_SITE_DIR/*
+
 
