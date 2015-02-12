f090469275b2e6aaecaa5af366c3c583491ddfcd
vshcherb <victor.shcherb@gmail.com>
2015-02-12 23:58:02 +0100

    Update update_site.sh

--- update_site.sh
+++ update_site.sh
@@ -9,12 +9,12 @@ DVR_SITE_DIR=/var/www-dvr/
 mkdir -p $LOCAL_SITE_DIR/
 cp -vur $GIT_SITE_DIR/* $LOCAL_SITE_DIR 
 cp -vu $GIT_SITE_DIR/../../resources/countries-info/countries.xml $LOCAL_SITE_DIR/countries.xml
-cp -vur ../help/website/* $LOCAL_SITE_DIR
+cp -vur ../../help/website/* $LOCAL_SITE_DIR
 chgrp -R www-data $LOCAL_SITE_DIR/*
 
 chmod g+w $LOCAL_SITE_DIR/indexes.xml 
 
-cp -vur ../help/dvr/* $DVR_SITE_DIR
+cp -vur ../../help/dvr/* $DVR_SITE_DIR
 chgrp -R www-data $LOCAL_SITE_DIR/*
 
 
