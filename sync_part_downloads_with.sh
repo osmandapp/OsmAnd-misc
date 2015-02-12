7cc3fdb438c55f386765daea7d1f7505c8ec7cd5
vshcherb <victor.shcherb@gmail.com>
2015-02-13 00:03:22 +0100

    Create sync_part_downloads_with.sh

---
+++ sync_part_downloads_with.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+function sync {
+	rsync --progress --delete-after --dirs --times jenkins@download.osmand.net:$1 $1
+}
+sync /var/www-download/indexes/
+sync /var/www-download/road-indexes/
