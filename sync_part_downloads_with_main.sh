ac5beba0ef798ff40189b57b53c1820c4c595aad
vshcherb <victor.shcherb@gmail.com>
2015-02-13 00:01:45 +0100

    Delete sync_part_downloads_with_main.sh

--- sync_part_downloads_with_main.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-function sync {
-	rsync --progress --delete-after --dirs --times jenkins@download.osmand.net:$1 $1
-}
-sync /var/www-download/indexes/
-sync /var/www-download/road-indexes/
-
