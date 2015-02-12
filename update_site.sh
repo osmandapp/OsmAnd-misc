1b04ad7a035b607311c22fd9132bf19bef03d8ba
vshcherb <victor.shcherb@gmail.com>
2015-02-12 23:50:57 +0100

    Update update_site.sh

--- update_site.sh
+++ update_site.sh
@@ -3,15 +3,15 @@ DIRECTORY=$(cd `dirname $0` && pwd)
 
 GIT_SITE_DIR=$DIRECTORY/site/
 LOCAL_SITE_DIR=/var/www-download/
+DVR_SITE_DIR=/var/www-dvr/
+
 
 mkdir -p $LOCAL_SITE_DIR/
-mkdir -p $LOCAL_SITE_DIR/static/
-cp -vur $GIT_SITE_DIR/* $LOCAL_SITE_DIR && \
-cp -vur $DIRECTORY/../help/website/* $LOCAL_SITE_DIR/ && \
-cp -vu $GIT_SITE_DIR/../../resources/countries-info/countries.xml $LOCAL_SITE_DIR/countries.xml && \
+cp -vur $GIT_SITE_DIR/* $LOCAL_SITE_DIR 
+cp -vu $GIT_SITE_DIR/../../resources/countries-info/countries.xml $LOCAL_SITE_DIR/countries.xml
+cp -vur ../help/website/* $LOCAL_SITE_DIR
 chgrp -R www-data $LOCAL_SITE_DIR/*
 chmod g+w $LOCAL_SITE_DIR/indexes.xml 
-#files='*.php tile_sources.xml favicon.ico'
-#for f in $files ; do
-#	cp $GIT_SITE_DIR/$f $LOCAL_SITE_DIR/ -u;
-#done
+
+cp -vur ../help/dvr/* $DVR_SITE_DIR
+
