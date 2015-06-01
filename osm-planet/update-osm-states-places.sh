2b4f3fab931bdf570216eb5a30bab15805adf76d
xmd5a <lornaderek@mail.ru>
2015-06-01 22:50:44 +0300

    Remove comment

--- osm-planet/update-osm-states-places.sh
+++ osm-planet/update-osm-states-places.sh
@@ -1,5 +1,1 @@
 wget -O osm-data/states_places.osm --post-file=osm-data/queries/states_places.txt "http://overpass-api.de/api/interpreter"
-
-# delete!!  node id="2912811859"
-#    tag k="name" v="Maryland"
-# this state brokes translations for US state Maryland
