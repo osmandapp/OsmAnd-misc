2fb5da07cd4157491226926ca542b59571ea6652
xmd5a <lornaderek@mail.ru>
2015-06-13 00:55:47 +0300

    Update osm-data. China Xinjiang and Tibet subregions polys.

--- osm-planet/update-osm-countries-admin-level-2.sh
+++ osm-planet/update-osm-countries-admin-level-2.sh
@@ -1,5 +1,7 @@
-wget -O osm-data/countries_admin_level_2.osm --post-file=osm-data/queries/countries_admin_level_2.txt "http://overpass-api.de/api/interpreter"
+NAME="countries_admin_level_2"
+wget -O osm-data/$NAME.osm --post-file=osm-data/queries/$NAME.txt "http://overpass-api.de/api/interpreter"
 
-sed -i "s/<\/osm>//g" osm-data/countries_admin_level_2.osm
-cat osm-data/queries/countries_admin_level_2-addition.txt >> osm-data/countries_admin_level_2.osm
-echo "</osm>" >> osm-data/countries_admin_level_2.osm
+sed -i "s/<\/osm>//g" osm-data/$NAME.osm
+cat osm-data/queries/$NAME-addition.txt >> osm-data/$NAME.osm
+echo "</osm>" >> osm-data/$NAME.osm
+sed -i '/member type=/d' ./osm-data/$NAME.osm
