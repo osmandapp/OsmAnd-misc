7a8d0ac2b2f02e4c8abcd405d93b24622eddf5e8
xmd5a <lornaderek@mail.ru>
2015-09-03 14:55:32 +0300

    Update translations (osm-data)

---
+++ osm-planet/update-osm-all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sh update-osm-countries-admin-level-2.sh
+sh update-osm-countries-places.sh
+sh update-osm-states-places.sh
+sh update-osm-states-regions.sh
--- osm-planet/update-osm-countries-places.sh
+++ osm-planet/update-osm-countries-places.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 wget -O osm-data/countries_places.osm --post-file=osm-data/queries/countries_places.txt "http://overpass-api.de/api/interpreter"
