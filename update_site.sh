78ce305c6d0cc3fb7024c5f59e1d06e3e17397d6
Victor Shcherb <victor.shcherb@gmail.com>
2015-11-07 18:58:51 +0100

    Update site

--- update_site.sh
+++ update_site.sh
@@ -12,6 +12,10 @@ cp -vur $GIT_SITE_DIR/* $LOCAL_SITE_DIR
 cp -vu $GIT_SITE_DIR/../../resources/countries-info/countries.xml $LOCAL_SITE_DIR/countries.xml
 
 cp -vur help/website/* $LOCAL_SITE_DIR
+## LEGACY
+cp -vu help/website/help/map-legend.html $LOCAL_SITE_DIR/help/Map-Legend_default.html
+cp -vu help/website/help/technical-articles.html $LOCAL_SITE_DIR/help/TechnicalArticles.html
+cp -vu help/website/help/legacy/HowToArticles.html $LOCAL_SITE_DIR/help/HowToArticles.html
 
 chgrp -R www-data $LOCAL_SITE_DIR/*
 chmod g+w $LOCAL_SITE_DIR/indexes.xml 
