e3dab782c1a172a93fc8bb5c06dbf55d05f70449
Victor Shcherb <victor.shcherb@gmail.com>
2015-11-23 22:42:41 +0100

    Update sync part downloads

--- sync_part_downloads_with_main.sh
+++ sync_part_downloads_with_main.sh
@@ -3,5 +3,5 @@ function sync {
 	rsync --progress --delete-after --dirs --times jenkins@download.osmand.net:$1 $1
 }
 sync /var/www-download/indexes/
-sync /var/www-download/road-indexes/
-sync /var/www-download/wiki/
+#sync /var/www-download/road-indexes/
+#sync /var/www-download/wiki/
