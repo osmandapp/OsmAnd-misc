973ab0234f0b9abd1f7af16c1257d22e81c6468c
xmd5a <lornaderek@mail.ru>
2017-04-10 17:23:45 +0300

    Fix fix_duplicate_obf

--- fix_duplicate_obf.sh
+++ fix_duplicate_obf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo Fix duplicate obf in indexes/*.obf.zip
-dir=/var/www-download/indexes
+dir=/var/lib/jenkins/indexes/uploaded/
 if [[ ! -d "$dir/tmp_dup" ]] ; then
 	mkdir "$dir/tmp_dup"
 fi
-rm -rf $dir/tmp_dup/*_2.*
+rm -rf $dir/tmp_dup/*.obf
+rm -rf $dir/tmp_dup/*.zip
 for file in $dir/*.obf.zip ; do
 	filename=$(basename $file)
 	count_obf=$(unzip -l $file | sed '/.zip/d' | grep obf | wc -l)
@@ -12,9 +12,7 @@ for file in $dir/*.obf.zip ; do
 		echo "Error! $file contains $count_obf obf files"
 		mv $file $dir/tmp_dup
 		unzip -d $dir/tmp_dup $dir/tmp_dup/$filename
-		echo $dir/${filename} $dir/tmp_dup/${filename%.*}
-		cd $dir/tmp_dup
-		zip ../${filename} ${filename%.*}
+		cp $dir/tmp_dup/${filename%.*} $dir/../
 		rm $dir/tmp_dup/*.obf
 	fi
 done
