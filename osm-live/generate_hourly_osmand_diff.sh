5cdf56fc6270a4315568c1e5ef5e3dc3b6590e44
Victor Shcherb <victor.shcherb@gmail.com>
2017-08-24 21:15:33 +0200

    Update script to generate obf diff

--- osm-live/generate_hourly_osmand_diff.sh
+++ osm-live/generate_hourly_osmand_diff.sh
@@ -14,8 +14,8 @@ while [ ! "$END_DAY $END_TIME" ==  "$START_DAY $START_TIME" ]; do
 
 
 START_DATE="${START_DAY}T${START_TIME}:00Z"
-
 NEXT="$START_DAY $START_TIME 5 minutes"
+
 NSTART_TIME=$(date +'%H' -d "$NEXT"):$(date +'%M' -d "$NEXT")
 NSTART_DAY=$(date +'%Y' -d "$NEXT")-$(date +'%m' -d "$NEXT")-$(date +'%d' -d "$NEXT")
 END_DATE="${NSTART_DAY}T${NSTART_TIME}:00Z"
@@ -24,6 +24,7 @@ END_DATE="${NSTART_DAY}T${NSTART_TIME}:00Z"
 #FILENAME_END="$(echo $END_DATE | tr '-' _)"
 FILENAME_START=Diff-start
 FILENAME_END=Diff-end
+FILENAME_DIFF="$(echo $START_DATE | tr '-' _)"
 
 DB_SEC=$(date -u --date="$(curl http://builder.osmand.net:8081/api/timestamp)" "+%s")
 END_SEC=$(date -u --date="$END_DATE" "+%s")
@@ -81,16 +82,13 @@ fi
 OsmAndMapCreator/utilities.sh generate-map $FILENAME_START.osm
 OsmAndMapCreator/utilities.sh generate-map $FILENAME_END.osm
 
-java -XX:+UseParallelGC -Xmx8096M -Xmn256M \
--Djava.util.logging.config.file=tools/obf-generation/batch-logging.properties \
--cp "OsmAndMapCreator/OsmAndMapCreator.jar:OsmAndMapCreator/lib/OsmAnd-core.jar:OsmAndMapCreator/lib/*.jar" \
-net.osmand.data.diff.DailyDiffGenerator \
--gen $FILENAME_START.obf $FILENAME_END.obf $RESULT_DIR
+OsmAndMapCreator/utilities.sh generate-obf-diff \
+$FILENAME_START.obf $FILENAME_END.obf $FILENAME_DIFF.diff.obf
 
+mv $FILENAME_DIFF.diff.obf $RESULT_DIR/
 rm -r *.osm
 rm -r *.obf
 
 START_DAY=$NSTART_DAY
 START_TIME=$NSTART_TIME
 done
-
