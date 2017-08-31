56a04e7c33b65d5fe06ea7d26357812cbc2c6920
Victor Shcherb <victor.shcherb@gmail.com>
2017-08-31 22:27:25 +0200

    Update script to run more frequent

--- osm-live/generate_hourly_osmand_diff.sh
+++ osm-live/generate_hourly_osmand_diff.sh
@@ -1,49 +1,47 @@
 #!/bin/bash -xe
 RESULT_DIR="/home/osm-planet/osmlive"
-START=($1)
-END=($2)
-echo "Current timestamp $END"
-echo "Processed timestamp $START"
-END_DAY=${END[0]}
-END_TIME=${END[1]}
-START_DAY=${START[0]}
-START_TIME=${START[1]}
+
 export JAVA_OPTS="-Xms128M -Xmx2014M"
-
 chmod +x OsmAndMapCreator/utilities.sh
-while [ ! "$END_DAY $END_TIME" ==  "$START_DAY $START_TIME" ]; do
 
-
-START_DATE="${START_DAY}T${START_TIME}:00Z"
-NEXT="$START_DAY $START_TIME 15 minutes"
-
-NSTART_TIME=$(date +'%H' -d "$NEXT"):$(date +'%M' -d "$NEXT")
-NSTART_DAY=$(date +'%Y' -d "$NEXT")-$(date +'%m' -d "$NEXT")-$(date +'%d' -d "$NEXT")
-END_DATE="${NSTART_DAY}T${NSTART_TIME}:00Z"
-
-#FILENAME_START="$(echo $START_DATE | tr '-' _)"
-#FILENAME_END="$(echo $END_DATE | tr '-' _)"
-FILENAME_START=Diff-start
-FILENAME_END=Diff-end
-FILENAME_DIFF="$(echo $NSTART_DAY-${NSTART_TIME} | tr '-' _ | tr ':' _ )"
-DATE_FOLDER_NAME="$(echo ${NSTART_DAY:2} | tr '-' _ | tr ':' _ )"
-TIME_FILE_SUFFIX="$(echo $NSTART_TIME | tr '-' _ | tr ':' _ )"
-
-FINAL_FOLDER=$RESULT_DIR/_diff/$NSTART_DAY/
-FINAL_FILE=$FINAL_FOLDER/$FILENAME_DIFF.obf.gz
-mkdir -p $FINAL_FOLDER/src/
-
+# database timestamp
 DB_SEC=$(date -u --date="$(curl http://builder.osmand.net:8081/api/timestamp)" "+%s")
-END_SEC=$(date -u --date="$END_DATE" "+%s")
-if [ $END_SEC \> $DB_SEC ]; then      
-  echo "END date is in the future of database!!!"
-  exit 1;
-fi;
+# CURRENT_SEC=$(date -u "+%s")
+START=$(cat /home/osm-planet/osmlive/.proc_timestamp)
+echo "Begin with timestamp: $START"
 
-if [ ! -f $FINAL_FILE ]; then
+while true; do
 
-  echo "Query between $START_DATE and $END_DATE"
-QUERY_START="
+  START_DATE="${START_DAY}T${START_TIME}:00Z"
+  NEXT="$START_DAY $START_TIME 15 minutes"
+
+  NSTART_TIME=$(date +'%H' -d "$NEXT"):$(date +'%M' -d "$NEXT")
+  NSTART_DAY=$(date +'%Y' -d "$NEXT")-$(date +'%m' -d "$NEXT")-$(date +'%d' -d "$NEXT")
+  END_DATE="${NSTART_DAY}T${NSTART_TIME}:00Z"
+
+  #FILENAME_START="$(echo $START_DATE | tr '-' _)"
+  #FILENAME_END="$(echo $END_DATE | tr '-' _)"
+  FILENAME_START=Diff-start
+  FILENAME_END=Diff-end
+  FILENAME_DIFF="$(echo $NSTART_DAY-${NSTART_TIME} | tr '-' _ | tr ':' _ )"
+  DATE_FOLDER_NAME="$(echo ${NSTART_DAY:2} | tr '-' _ | tr ':' _ )"
+  TIME_FILE_SUFFIX="$(echo $NSTART_TIME | tr '-' _ | tr ':' _ )"
+  
+  FINAL_FOLDER=$RESULT_DIR/_diff/$NSTART_DAY/
+  FINAL_FILE=$FINAL_FOLDER/$FILENAME_DIFF.obf.gz
+  mkdir -p $FINAL_FOLDER/src/
+  
+  
+  END_SEC=$(date -u --date="$END_DATE" "+%s")
+  if [ $END_SEC \> $DB_SEC ]; then      
+    echo "END date $END_DATE is in the future of database!!!"
+    exit 0;
+  fi;
+
+  if [ ! -f $FINAL_FILE ]; then
+
+    echo "Query between $START_DATE and $END_DATE"
+    QUERY_START="
 [timeout:1800][maxsize:2000000000]
 [date:\"$START_DATE\"];
 (
@@ -58,7 +56,7 @@ QUERY_START="
 (node(w.a);.a) ->.a;
 	.a out geom meta;
 "
-QUERY_END="
+    QUERY_END="
 [timeout:1800][maxsize:2000000000]
 [date:\"$END_DATE\"];
 (
@@ -73,51 +71,53 @@ QUERY_END="
 (node(w.a);.a) ->.a;
 	.a out geom meta;
 "
-#if [ ! -f $FILENAME_START.osm ]; then
-  echo $QUERY_START | /home/overpass/osm3s/bin/osm3s_query > $FILENAME_START.osm
-  TZ=UTC touch -c -d "$START_DATE" $FILENAME_START.osm
-#fi
-if ! grep -q "<\/osm>"  $FILENAME_START.osm; then
-    rm $FILENAME_START.osm;
-    exit 1;
-fi
-#if [ ! -f $FILENAME_END.osm ]; then
-  echo $QUERY_END | /home/overpass/osm3s/bin/osm3s_query  > $FILENAME_END.osm
-  TZ=UTC touch -c -d "$END_DATE" $FILENAME_END.osm 
-#fi
-if ! grep -q "<\/osm>"  $FILENAME_END.osm; then
-    rm $FILENAME_END.osm;
-    exit 1;
-fi
+    #if [ ! -f $FILENAME_START.osm ]; then
+      echo $QUERY_START | /home/overpass/osm3s/bin/osm3s_query > $FILENAME_START.osm
+      TZ=UTC touch -c -d "$START_DATE" $FILENAME_START.osm
+    #fi
+    if ! grep -q "<\/osm>"  $FILENAME_START.osm; then
+        rm $FILENAME_START.osm;
+        exit 1;
+    fi
+    #if [ ! -f $FILENAME_END.osm ]; then
+      echo $QUERY_END | /home/overpass/osm3s/bin/osm3s_query  > $FILENAME_END.osm
+      TZ=UTC touch -c -d "$END_DATE" $FILENAME_END.osm 
+    #fi
+    if ! grep -q "<\/osm>"  $FILENAME_END.osm; then
+        rm $FILENAME_END.osm;
+        exit 1;
+    fi
   
 
   
-  TZ=UTC touch -c -d "$END_DATE" $FILENAME_START.osm
-  TZ=UTC touch -c -d "$END_DATE" $FILENAME_END.osm
-  OsmAndMapCreator/utilities.sh generate-obf-no-address $FILENAME_START.osm
-  OsmAndMapCreator/utilities.sh generate-obf-no-address $FILENAME_END.osm
+    TZ=UTC touch -c -d "$END_DATE" $FILENAME_START.osm
+    TZ=UTC touch -c -d "$END_DATE" $FILENAME_END.osm
+    OsmAndMapCreator/utilities.sh generate-obf-no-address $FILENAME_START.osm
+    OsmAndMapCreator/utilities.sh generate-obf-no-address $FILENAME_END.osm
+  
+    OsmAndMapCreator/utilities.sh generate-obf-diff \
+    $FILENAME_START.obf $FILENAME_END.obf $FILENAME_DIFF.diff.obf
+  
+    gzip -c $FILENAME_DIFF.diff.obf > $FINAL_FILE
+    TZ=UTC touch -c -d "$END_DATE" $FINAL_FILE
+    gzip -c $FILENAME_START.obf > $FINAL_FOLDER/src/${FILENAME_DIFF}_before.obf.gz
+    gzip -c $FILENAME_END.obf > $FINAL_FOLDER/src/${FILENAME_DIFF}_after.obf.gz
+    #gzip -c $FILENAME_START.osm > $FINAL_FOLDER/src/${FILENAME_DIFF}_before.osm.gz
+    #gzip -c $FILENAME_END.osm > $FINAL_FOLDER/src/${FILENAME_DIFF}_after.osm.gz
+  
+  
+    OsmAndMapCreator/utilities.sh split-obf \
+    $FILENAME_DIFF.diff.obf $RESULT_DIR  \
+    OsmAndMapCreator/regions.ocbf "$DATE_FOLDER_NAME" "_$TIME_FILE_SUFFIX"
+  
+  
+    rm -r *.osm || true
+    rm -r *.rtree* || true
+    rm -r *.obf || true
+  fi 
 
-  OsmAndMapCreator/utilities.sh generate-obf-diff \
-  $FILENAME_START.obf $FILENAME_END.obf $FILENAME_DIFF.diff.obf
+  START_DAY=$NSTART_DAY
+  START_TIME=$NSTART_TIME
 
-  gzip -c $FILENAME_DIFF.diff.obf > $FINAL_FILE
-  TZ=UTC touch -c -d "$END_DATE" $FINAL_FILE
-  gzip -c $FILENAME_START.obf > $FINAL_FOLDER/src/${FILENAME_DIFF}_before.obf.gz
-  gzip -c $FILENAME_END.obf > $FINAL_FOLDER/src/${FILENAME_DIFF}_after.obf.gz
-  #gzip -c $FILENAME_START.osm > $FINAL_FOLDER/src/${FILENAME_DIFF}_before.osm.gz
-  #gzip -c $FILENAME_END.osm > $FINAL_FOLDER/src/${FILENAME_DIFF}_after.osm.gz
-
-
-  OsmAndMapCreator/utilities.sh split-obf \
-   $FILENAME_DIFF.diff.obf $RESULT_DIR  \
-   OsmAndMapCreator/regions.ocbf "$DATE_FOLDER_NAME" "_$TIME_FILE_SUFFIX"
-
-
-  rm -r *.osm || true
-  rm -r *.rtree* || true
-  rm -r *.obf || true
-fi 
-
-START_DAY=$NSTART_DAY
-START_TIME=$NSTART_TIME
+  echo "$NSTART_DAY $NSTART_TIME" > /home/osm-planet/osmlive/.proc_timestamp
 done
