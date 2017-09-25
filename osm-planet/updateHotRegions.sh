a7989d499a3152878cd66a4a674503169f0ae1cd
xmd5a <lornaderek@mail.ru>
2017-09-26 00:23:14 +0300

    Fix updateHotRegions.sh

--- osm-planet/updateHotRegions.sh
+++ osm-planet/updateHotRegions.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-BASE=/var/lib/jenkins/osm-planet/regions
-PBF_FILES="/home/osm-planet/osmc/"
-
+BASE="/var/lib/jenkins/hot-regions"
+PBF_FILES="/home/osm-planet/osm-extract"
 
 updateRegion() {
 	echo "Update ${1} using ${2}"
 	FILE="${BASE}/${1}.o5m"
 	if [ ! -f $FILE ]; then
-		osmconvert --out-o5m "${PBF_FILES}/${3}" -B=misc/osm-planet/${2} > ${FILE} 
+		osmconvert --out-o5m "${PBF_FILES}/${3%%.*}/${3}" -B=misc/osm-planet/${2} > ${FILE} 
 	fi
-    osmupdate $FILE ${BASE}/current-update.o5m -B=misc/osm-planet/${2} -v
+    osmupdate $FILE $(osmconvert $FILE --out-statistics | grep 'timestamp max' | sed 's/timestamp max: //g') ${BASE}/current-update.o5m -B=misc/osm-planet/${2} -v
     osmconvert --out-pbf $FILE > ${BASE}/${1}.osm.pbf
     mv -f $FILE ${FILE}.old || true
     # rm ${BASE}/${1}.o5m
@@ -19,4 +18,7 @@ updateRegion() {
 rm current-update.o5m || true
 
 #HOT OSM
+updateRegion Mexico polygons/north-america/mexico.poly mexico_northamerica.pbf
+updateRegion Mexico_nuevo-leon polygons/north-america/mexico/nuevo-leon.poly mexico_nuevo-leon_northamerica.pbf
+
 # updateRegion Nepal polygons/east-asia/nepal.poly nepal.pbf
