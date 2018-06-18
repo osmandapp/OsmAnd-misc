5e9d6b4beccc8c48328c2d139fbbe8867747325c
Victor Shcherb <victor.shcherb@gmail.com>
2018-06-18 10:34:50 +0200

    Add srtm data to osm live

--- fix_duplicate_obf.sh
+++ fix_duplicate_obf.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 dir=/var/lib/jenkins/indexes/uploaded/
 if [[ ! -d "$dir/tmp_dup" ]] ; then
-	mkdir "$dir/tmp_dup"
+        mkdir "$dir/tmp_dup"
 fi
 rm -rf $dir/tmp_dup/*.obf
 rm -rf $dir/tmp_dup/*.zip
 for file in $dir/*.obf.zip ; do
-	filename=$(basename $file)
-	count_obf=$(unzip -l $file | sed '/.zip/d' | grep obf | wc -l)
-	if [[ $count_obf > 1 ]] ; then
-		echo "Error! $file contains $count_obf obf files"
-		mv $file $dir/tmp_dup
-		unzip -d $dir/tmp_dup $dir/tmp_dup/$filename
-		cp $dir/tmp_dup/${filename%.*} $dir/../
-		rm $dir/tmp_dup/*.obf
-	fi
+        filename=$(basename $file)
+        count_obf=$(unzip -l $file | sed '/.zip/d' | grep obf | wc -l)
+        if [[ $count_obf > 1 ]] ; then
+                echo "Error! $file contains $count_obf obf files"
+                mv $file $dir/tmp_dup
+                unzip -d $dir/tmp_dup $dir/tmp_dup/$filename
+                cp $dir/tmp_dup/${filename%.*} $dir/../
+                rm $dir/tmp_dup/*.obf
+        fi
 done
+rmdir "$dir/tmp_dup"
