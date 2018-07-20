13f94d5fba4c316f7569476af444e60688390310
vshcherb <victor.shcherb@gmail.com>
2018-07-20 19:15:01 +0200

    Update sync.sh

--- config/nginx/sync.sh
+++ config/nginx/sync.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 function sync {
         echo $(date) Sync $1 >> ~/rsync.log
-        /root/rsync-3.1.2/rsync -og --chown=www-data:www-data --delete --progress --recursive --times --log-file ~/rsync.log root@dl6.osmand.net:/mnt/media/$1/ /media/content/$1
+        /root/rsync-3.1.2/rsync -og --chown=www-data:www-data --delete --progress --recursive --times --log-file ~/rsync.log root@dl6.osmand.net:/media/content/$1/ /media/content/$1
         # && chown -R www-data:www-data $1
 }
 sync indexes
 # sync hillshade
 # sync srtm-countries
+# sync releases
+# sync osm-releases
 sync wikivoyage
 sync road-indexes
 sync wiki
