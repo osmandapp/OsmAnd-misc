7b68566b0d734fd524455d10be2e8472d26d3b98
Victor Shcherb <victor.shcherb@gmail.com>
2018-09-04 21:22:22 +0200

    Add static support for different files

--- sync_downloads_with_main.sh
+++ sync_downloads_with_main.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 function sync {
-	rsync --progress --delete-after --dirs --times jenkins@download.osmand.net:$1 $1
+	rsync -r --progress --delete-after --dirs --times jenkins@download.osmand.net:$1 $1
 }
 sync /var/www-download/indexes/
 sync /var/www-download/hillshade/
 sync /var/www-download/srtm-countries/
 sync /var/www-download/road-indexes/
 sync /var/www-download/wiki/
+sync /var/www-download/wikivoyage/
