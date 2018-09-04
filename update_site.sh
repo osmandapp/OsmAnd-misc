271d6bdff642878f468318264c50a54b08979137
Victor Shcherb <victor.shcherb@gmail.com>
2018-09-04 14:53:39 +0200

    Change how site is updated

--- update_site.sh
+++ update_site.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 DIRECTORY=$(cd `dirname $0` && pwd)
-
-GIT_SITE_DIR=$DIRECTORY/site/
 if [ -z "$LOCAL_SITE_DIR" ]; then
 	LOCAL_SITE_DIR=/var/www-download/
 fi
 mkdir -p $LOCAL_SITE_DIR/
-cp -vur $GIT_SITE_DIR/* $LOCAL_SITE_DIR 
-# cp -vu $GIT_SITE_DIR/../../resources/countries-info/countries.xml $LOCAL_SITE_DIR/countries.xml
+# (if doesn't exist) git clone https://github.com/osmandapp/osmandapp.github.io.git $LOCAL_SITE_DIR
 
 mkdir -p $LOCAL_SITE_DIR/hillshade
 mkdir -p $LOCAL_SITE_DIR/indexes
@@ -45,15 +42,5 @@ mkdir -p $LOCAL_SITE_DIR/gen
 cp misc/config/nginx-main/*.conf /etc/nginx/server-include/ || true
 sudo service nginx reload || true
 
-cp -vur help/website/* $LOCAL_SITE_DIR
-rsync -arv --delete help/website/ $LOCAL_SITE_DIR/web/
-# curl -fSL https://github.com/osmandapp/OsmAnd-resources/raw/master/countries-info/regions.ocbf > $LOCAL_SITE_DIR/regions_v2.ocbf
-
-## LEGACY
-# cp -vu help/website/help/map-legend.html $LOCAL_SITE_DIR/help/Map-Legend_default.html
-# cp -vu help/website/help/technical-articles.html $LOCAL_SITE_DIR/help/TechnicalArticles.html
-# cp -vu help/website/help/legacy/HowToArticles.html $LOCAL_SITE_DIR/help/HowToArticles.html
-
-
 chgrp -R www-data $LOCAL_SITE_DIR/*
-chmod g+w $LOCAL_SITE_DIR/indexes.xml 
+cd $LOCAL_SITE_DIR  && git pull
