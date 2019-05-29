a50464d27f9e12b6f3d77cbff35c3e4404f87a37
Victor Shcherb <victor.shcherb@gmail.com>
2019-05-29 18:10:05 +0200

    Add vars

--- config/mapnik_update.sh
+++ config/mapnik_update.sh
@@ -6,6 +6,7 @@ OSMOSIS=${OSMOSIS:-$FOLDER/osmosis.run}
 DB_PORT=${DB_PORT:-5433}
 TILES_DIR=${TILES_DIR:-/var/lib/tirex/tiles/}
 TILES_SOCK=${TILES_SOCK:-/var/lib/tirex/modtile.sock}
+OSM_STYLE=${OSM_STYLE:/usr/local/share/osm2pgsql/default.style}
 
 ID=$(date +"%d_%m_%H_%M")
 CHANGES_FILE=$FOLDER/changes_$ID.osc.gz
@@ -25,7 +26,7 @@ cp $FOLDER/osmosis-workdir/state-old.txt $FOLDER/osmosis-workdir/state.txt
 # -U jenkins
 osm2pgsql --append --slim -d $DB_NAME -P $DB_PORT --cache-strategy dense \
 	--cache 20000 --number-processes 4 --hstore \
- 	--style /usr/local/share/osm2pgsql/default.style  --multi-geometry \
+ 	--style $OSM_STYLE --multi-geometry \
  	--flat-nodes $FOLDER/flatnodes.bin \
 	--expire-tiles 13-18 --expire-output $EXPIRED_FILE $CHANGES_FILE
 cp $FOLDER/osmosis-workdir/state-new.txt $FOLDER/osmosis-workdir/state.txt
