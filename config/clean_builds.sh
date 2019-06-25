2f4ae097b95a54429cdfed5d1b9534b0c6f06f77
vshcherb <victor.shcherb@gmail.com>
2019-06-26 01:35:13 +0200

    Update clean_builds.sh

--- config/clean_builds.sh
+++ config/clean_builds.sh
@@ -7,7 +7,7 @@ for f in *-20*; do
     if [ ! -z "$sc" ]; then
         dow=$(date --date="20$fdate" +%u);
         # more than 14 days keep 50%
-        delete=0;
+        delete=false;
         if (($scnow - $sc > 14*60*60*24)); then
             delete=$(( $dow == 2 || $dow == 4 || $dow == 7 ));
         elif (($scnow - $sc > 28*60*60*24)); then
@@ -15,7 +15,7 @@ for f in *-20*; do
         elif (($scnow - $sc > 90*60*60*24)); then
             delete=$(( $dow == 2 || $dow == 3 || $dow == 4 || $dow == 6 || $dow == 7 ));
         fi
-        if  (( $delete )); then
+        if $delete; then
             echo "Deleting $f: $dow $fdate";
         fi
     fi
