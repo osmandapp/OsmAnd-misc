7450336239a27a1bac98697b4bc796c9c8da578c
vshcherb <victor.shcherb@gmail.com>
2019-06-26 01:34:23 +0200

    Update clean_builds.sh

--- config/clean_builds.sh
+++ config/clean_builds.sh
@@ -15,7 +15,7 @@ for f in *-20*; do
         elif (($scnow - $sc > 90*60*60*24)); then
             delete=$(( $dow == 2 || $dow == 3 || $dow == 4 || $dow == 6 || $dow == 7 ));
         fi
-        if  $delete; then
+        if  (( $delete )); then
             echo "Deleting $f: $dow $fdate";
         fi
     fi
