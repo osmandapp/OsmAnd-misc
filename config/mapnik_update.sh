d11e5ba4be7389b2b81563f5b99a7632823b5a02
Victor Shcherb <victor.shcherb@gmail.com>
2020-04-19 14:13:13 +0200

    Remove changes file so it doesn't get stuck on wrong file

--- config/mapnik_update.sh
+++ config/mapnik_update.sh
@@ -22,7 +22,7 @@ echo "CURRENT STATE: "
 cat "$STATE_FOLDER/state.txt"
 cp $STATE_FOLDER/state.txt $STATE_FOLDER/state-old.txt
 
-rm $CHANGES_FILE # comment to keep same file for processing
+rm $CHANGES_FILE || true # comment to keep same file for processing
 if [ ! -f "$CHANGES_FILE" ] ; then 
 	TIMESTAMP=$(cat "$STATE_FOLDER/state.txt")
 	osmupdate -v $TIMESTAMP $CHANGES_FILE
