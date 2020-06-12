7ae993224ec54e44c351e61475cd6fdb97a8d3ab
xmd5a <lornaderek@mail.ru>
2020-06-12 15:41:11 +0300

    Norway new regions. Update translations

--- osm-planet/update-osm-states-regions.sh
+++ osm-planet/update-osm-states-regions.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-wget -O osm-data/states_regions.osm --post-file=osm-data/queries/states_regions.txt "http://overpass-api.de/api/interpreter"
+wget -O osm-data/states_regions.osm --post-file=osm-data/queries/states_regions.txt "https://z.overpass-api.de/api/interpreter"
 sed -i '/member type=/d' ./osm-data/states_regions.osm
+sed -i '0,/k="name" v="Bas-Rhin"/{s/k="name" v="Bas-Rhin"/k="name" v="1"/}' ./osm-data/states_regions.osm
+sed -i '0,/k="name:en" v="Luzhou City"/{s/k="name:en" v="Luzhou City"/k="name:en" v="1"/}' ./osm-data/states_regions.osm
+sed -i '0,/k="name:en" v="Yibin City"/{s/k="name:en" v="Yibin City"/k="name:en" v="1"/}' ./osm-data/states_regions.osm
+sed -i '0,/k="name:en" v="Zhaotong City"/{s/k="name:en" v="Zhaotong City"/k="name:en" v="1"/}' ./osm-data/states_regions.osm
