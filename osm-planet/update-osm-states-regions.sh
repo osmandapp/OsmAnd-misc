02e1ac3ba4dea2513e03619637ff04e153493011
xmd5a <lornaderek@mail.ru>
2021-04-24 18:43:19 +0300

    Update translations (https://github.com/osmandapp/OsmAnd/issues/11457)

--- osm-planet/update-osm-states-regions.sh
+++ osm-planet/update-osm-states-regions.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget -O osm-data/states_regions.osm --post-file=osm-data/queries/states_regions.txt "https://z.overpass-api.de/api/interpreter"
 sed -i '/member type=/d' ./osm-data/states_regions.osm
+sed -i '0,/k="name:en" v="Aksai Chin"/{s/k="name:en" v="Aksai Chin"/k="name:en" v="1"/}' ./osm-data/states_regions.osm
