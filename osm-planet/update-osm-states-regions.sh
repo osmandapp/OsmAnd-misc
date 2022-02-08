8995a0143cb736ee2f6a7fd4d0944067317057c2
ivanPyrohivskyi <ivan.pirogovsky@gmail.com>
2022-02-08 14:33:09 +0200

    Add translates Flanders and Brussels, Brandenburg and Berlin

--- osm-planet/update-osm-states-regions.sh
+++ osm-planet/update-osm-states-regions.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-wget -O osm-data/states_regions.osm --post-file=osm-data/queries/states_regions.txt "https://z.overpass-api.de/api/interpreter"
-sed -i '/member type=/d' ./osm-data/states_regions.osm
-sed -i '0,/k="name:en" v="Aksai Chin"/{s/k="name:en" v="Aksai Chin"/k="name:en" v="1"/}' ./osm-data/states_regions.osm
+NAME="states_regions"
+wget -O osm-data/states_regions.osm --post-file=osm-data/queries/$NAME.txt "https://z.overpass-api.de/api/interpreter"
+sed -i "s/<\/osm>//g" osm-data/$NAME.osm
+cat osm-data/queries/$NAME-addition.txt >> osm-data/$NAME.osm
+sed -i '/member type=/d' ./osm-data/$NAME.osm
+sed -i '0,/k="name:en" v="Aksai Chin"/{s/k="name:en" v="Aksai Chin"/k="name:en" v="1"/}' ./osm-data/$NAME.osm
+echo "</osm>" >> osm-data/$NAME.osm
