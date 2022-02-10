7b1a720bb635b1b69c4cc924b079e827004fe93d
xmd5a <lornaderek@mail.ru>
2022-02-10 16:21:30 +0300

    Update translations and polys (https://github.com/osmandapp/OsmAnd/issues/13599)

--- osm-planet/update-osm-states-regions.sh
+++ osm-planet/update-osm-states-regions.sh
@@ -3,7 +3,7 @@ NAME="states_regions"
 wget -O osm-data/states_regions.osm --post-file=osm-data/queries/$NAME.txt "https://z.overpass-api.de/api/interpreter"
 sed -i "s/<\/osm>//g" osm-data/$NAME.osm
 sed -i '/member type=/d' ./osm-data/$NAME.osm
-sed -i '0,/k="name:en" v="Aksai Chin"/{s/k="name:en" v="Aksai Chin"/k="name:en" v="1"/}' ./osm-data/$NAME.osm
+#sed -i '0,/k="name:en" v="Aksai Chin"/{s/k="name:en" v="Aksai Chin"/k="name:en" v="1"/}' ./osm-data/$NAME.osm
 
 #Translates
 > osm-data/flanders_brussel.osm
