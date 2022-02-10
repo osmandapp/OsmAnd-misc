3ea68f787c7effea11fe8ef6a257fd75a171f045
Ivan <ivan.pirogovsky@gmail.com>
2022-02-10 12:54:44 +0200

    Add translates Flanders and Brussels, Brandenburg and Berlin (#132)
    * Add translates Flanders and Brussels, Brandenburg and Berlin
    
    * Automatic translates
    
    * Add generated states_regions.osm

--- osm-planet/update-osm-states-regions.sh
+++ osm-planet/update-osm-states-regions.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-wget -O osm-data/states_regions.osm --post-file=osm-data/queries/states_regions.txt "https://z.overpass-api.de/api/interpreter"
-sed -i '/member type=/d' ./osm-data/states_regions.osm
-sed -i '0,/k="name:en" v="Aksai Chin"/{s/k="name:en" v="Aksai Chin"/k="name:en" v="1"/}' ./osm-data/states_regions.osm
+NAME="states_regions"
+wget -O osm-data/states_regions.osm --post-file=osm-data/queries/$NAME.txt "https://z.overpass-api.de/api/interpreter"
+sed -i "s/<\/osm>//g" osm-data/$NAME.osm
+sed -i '/member type=/d' ./osm-data/$NAME.osm
+sed -i '0,/k="name:en" v="Aksai Chin"/{s/k="name:en" v="Aksai Chin"/k="name:en" v="1"/}' ./osm-data/$NAME.osm
+
+#Translates
+> osm-data/flanders_brussel.osm
+./combine_translations.sh osm-data/$NAME.osm 53134 54094 osm-data/flanders_brussel.osm
+cat osm-data/flanders_brussel.osm >> osm-data/$NAME.osm
+
+> osm-data/brandenburg_berlin.osm
+./combine_translations.sh osm-data/$NAME.osm 62504 62422 osm-data/brandenburg_berlin.osm
+cat osm-data/brandenburg_berlin.osm >> osm-data/$NAME.osm
+
+echo "</osm>" >> osm-data/$NAME.osm
